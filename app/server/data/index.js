d51e366add7328d5ef990cea43dffb27cdd661dd
Your Name <you@example.com>
2021-02-22 09:12:53 +0600

    created model, migration, route and controller for cluster, brand, cluster user and file

--- app/server/data/index.js
+++
@@ -1,7 +0,0 @@
-import androids from "./android.js";
-import cameras from "./camera.js";
-import laptops from "./laptop.js";
-
-const fakeData = [...androids, ...cameras, ...laptops];
-
-export default fakeData;
